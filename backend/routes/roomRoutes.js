7894881f5b74913f9e1f486f9268378bf7994c4a
Asaduzzaman9202 <asaduzzaman8316@gmail.com>
2025-12-09 19:48:49 +0600

    fix: Remove missing roomValidation middleware

--- backend/routes/roomRoutes.js
+++ backend/routes/roomRoutes.js
@@ -7,13 +7,13 @@ const {
     deleteRoom
 } = require('../controllers/roomController');
 const { protect, authorize } = require('../middleware/authMiddleware');
-const { roomValidation, idValidation } = require('../validators/validators');
+const { idValidation } = require('../validators/validators');
 
 // Public routes
 router.route('/').get(getRooms);
 
 // Protected routes (require admin)
-router.route('/').post(protect, authorize('admin'), roomValidation.create, createRoom);
-router.route('/:id').put(protect, authorize('admin'), roomValidation.update, updateRoom).delete(protect, authorize('admin'), idValidation, deleteRoom);
+router.route('/').post(protect, authorize('admin'), createRoom);
+router.route('/:id').put(protect, authorize('admin'), updateRoom).delete(protect, authorize('admin'), idValidation, deleteRoom);
 
 module.exports = router;
